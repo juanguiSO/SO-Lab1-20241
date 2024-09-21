c819dcb16cec0290599eb03acdbddc3a91dbe019
JoseMAGG <jose.gonzalezg1@udea.edu.co>
2024-09-20 22:29:13 -0500

    Test 2 completado

--- reverse.c
+++ reverse.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
-int main (int argc, char *argv[])
+#include <stdio.h>
+int main(int argc, char *argv[])
 {
+     // 1. Verificación de cantidad de argumentos en la línea de comandos
+     if (argc > 3)
+     {
+          fprintf(stderr, "usage: reverse <input> <output>\n");
+          return 1;
+     }
 
- // Verificación de cantidad de argumentos en la línea de comandos
-    if (argc == 3) {
-         return 0;
-       
-    }else{
-        fprintf(stderr, "usage: reverse <input> <output>\n");
-        return 1;
-        }
-
-   
-
+     // 2. Verificar que el archivo existe
+     FILE *fp;
+     fp = fopen(argv[1], "r");
+     if (!fp)
+     {
+          fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
+          return 1;
+     }
 }
